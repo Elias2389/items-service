ce7a22b652dad047d538a156272c857116bc6b9c
Andres <andres.rivas@mercadolibre.com>
2020-07-10 12:03:50 -0300

    Added request to get info from config server

--- src/main/java/com/ae/item/ItemApplication.java
+++ src/main/java/com/ae/item/ItemApplication.java
@@ -1,13 +1,15 @@
 package com.ae.item;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
-@EnableCircuitBreaker
+ @EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
--- src/main/java/com/ae/item/controller/ItemController.java
+++ src/main/java/com/ae/item/controller/ItemController.java
@@ -4,23 +4,39 @@ import com.ae.item.model.Item;
 import com.ae.item.model.Product;
 import com.ae.item.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.Properties;
+import java.util.Map;
 
+
+@RefreshScope
 @RestController
 public class ItemController {
 
     private final ItemService service;
 
+    @Autowired
     public ItemController(@Qualifier("ItemServiceFeing") ItemService service) {
         this.service = service;
     }
+
+    private static Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+    @Value("${config.text}")
+    private String text;
 
     @GetMapping("/item")
     public List<Item> findItems() {
@@ -43,7 +59,16 @@ public class ItemController {
         item.setCount(count);
         item.setProduct(product);
 
-
         return item;
+    }
+
+    @GetMapping("/config")
+    public ResponseEntity<?> getConfig(@Value("${server.port}") String port) {
+        logger.info(String.format("TEXT: %s", text));
+
+        Map<String, String> map = new HashMap<>();
+        map.put("text", text);
+        map.put("port", port);
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
