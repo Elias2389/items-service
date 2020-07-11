55ba03baa6bb29265435d8fb19c2976fe616d8df
Andres <andres.rivas@mercadolibre.com>
2020-07-11 08:59:44 -0300

    Added crud methods

--- src/main/java/com/ae/item/clients/ProductClientRest.java
+++ src/main/java/com/ae/item/clients/ProductClientRest.java
@@ -3,18 +3,44 @@ package com.ae.item.clients;
 import com.ae.item.model.Product;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @FeignClient(name = "product-service")
-    public interface ProductClientRest {
+public interface ProductClientRest {
 
+    /**
+     * @return List of products
+     */
     @GetMapping("/product")
     public List<Product> getProducts();
 
+    /**
+     * @param id of product
+     * @return product by id
+     */
     @GetMapping("/product/{id}")
-    public Product getProduct(@PathVariable final Long id );
+    public Product getProduct(@PathVariable final Long id);
+
+    /**
+     * @param product to create
+     * @return product created
+     */
+    @PostMapping("/product")
+    public Product createProduct(@RequestBody final Product product);
+
+    /**
+     * @param product to update
+     * @return product updated
+     */
+    @PutMapping("/product")
+    public Product updateProduct(@RequestBody final Product product);
+
+    /**
+     * @param id of product to delete
+     */
+    @DeleteMapping("/product/{id}")
+    public void deleteProduct(@PathVariable("id") final Long id);
 
 }
